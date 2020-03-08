b629af970dabc235504485e1a58e5c78d03328f5
Jen Miao <jenmiao@gmail.com>
2020-03-08 05:04:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,62 @@
-require 'pry'
+# require 'pry'
+#
+# class Artist
+#
+#   attr_accessor :artist, :name
+#
+#   def initialize(name)
+#     @name = name
+#   end
+#
+#   def songs
+#     Song.all.select do |song|
+#       song.artist == self # double equal bc we are testing true/false
+#     end
+#   end
+#
+#   def add_song(song)
+#     #binding.pry
+#     song.artist = self
+#   end
+#
+#   def add_song_by_name(name)
+#     new_song = Song.new(name)
+#     add_song(new_song)
+#   end
+#
+# #binding.pry
+#
+# end
+
 
 class Artist
+  attr_accessor :name
 
-  attr_accessor :artist, :name
+  @@song_count = 0
 
   def initialize(name)
     @name = name
-  end
-
-  def songs
-    Song.all.select do |song|
-      song.artist == self # double equal bc we are testing true/false
-    end
+    @songs = []
   end
 
   def add_song(song)
-    #binding.pry
+    self.songs << song
     song.artist = self
+    @@song_count +=1
   end
 
   def add_song_by_name(name)
-    new_song = Song.new(name)
-    add_song(new_song)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
   end
 
-#binding.pry
+  def songs
+    @songs
+  end
 
+  def self.song_count
+    @@song_count
+  end
 end
