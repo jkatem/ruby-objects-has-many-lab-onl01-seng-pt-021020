9330e849086fe71a95e42c8ba40058b0746b064c
Jen Miao <jenmiao@gmail.com>
2020-03-08 05:58:05 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,22 +1,17 @@
 
-# class Post
-#
-#   attr_accessor :title, :author
-#
-#   def initialize(title)
-#     @title = title
-#     @@all <<
-#   end
-#
-#   def all
-#     @@all
-#   end
-#
-#   def author_name
-#     if @author == nil
-#       return nil
-#     else
-#       @author.name
-#     end
-#   end
-# end
+
+class Post
+
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+end
